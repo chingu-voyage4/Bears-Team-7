7f25f46d92d1f3284fd58e494a30606752aacf9a
Matthew-Burfield <m1.burfield@gmail.com>
2018-03-14 09:13:25 +1000

    added editor component that accepts key inputs, also added prettier

---
+++ client/.prettierrc.js
@@ -0,0 +1,9 @@
+// .prettierrc.js
+
+module.exports = {
+  printWidth: 80,
+  parser: 'flow',
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+};
---
+++ client/src/components/Editor.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="editor" v-on:keyup="handleKeyboardPress" tabindex="0">
+    {{ documentData }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Editor',
+  data: () => ({
+    documentData: '',
+  }),
+  // define methods under the `methods` object
+  methods: {
+    handleKeyboardPress(event) {
+      this.documentData = this.documentData + event.key;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+.editor {
+  background-color: burlywood;
+  width: 500px;
+  height: 500px;
+}
+.editor:focus {
+  border: 1 solid black;
+}
+</style>
