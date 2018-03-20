bc5cca8c40e7f7430b15b9c62adb9052ceb22793
Matthew-Burfield <m1.burfield@gmail.com>
2018-03-20 18:49:54 +1000

    testing prettier/eslint precommit githook

--- client/.prettierrc.js
+++ client/.prettierrc.js
@@ -1,16 +1,4 @@
-// .prettierrc.js
-
 module.exports = {
-  overrides: [
-    {
-      files: "*.js",
-      options: {
-        printWidth: 80,
-        parser: "flow",
-        singleQuote: true,
-        trailingComma: "all",
-        arrowParens: "always"
-      }
-    }
-  ]
-};
+	singleQuote: true,
+	trailingComma: 'all',
+}
