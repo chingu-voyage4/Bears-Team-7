266c765a85f0b202d5c819a68f5db38044f3e271
Matthew Burfield <Matthew-Burfield@users.noreply.github.com>
2018-03-22 08:18:19 +1000

    Update Editor.spec.js
    You're totally right that .text() trims strings, I'm not sure the best way to test this then - potentially we could use the .html() and check that it contains two spaces?
    
    Otherwise, I've added a test that checks the documentData object directly (not usually good practice as it's testing the implementation details of the Vue component as opposed to the end result the user sees)
    
    Anyway, what do you think?

--- client/test/unit/specs/Editor.spec.js
+++ client/test/unit/specs/Editor.spec.js
@@ -17,6 +17,8 @@ describe('Editor.vue', () => {
     expect(wrapper.find('div[data-test="editor"]').text()).toBe('');
   });
 
+  const space = '\xa0';
+  const tab = space + space;
   const keyCodes = {
     a: {
       keyCode: 65,
@@ -99,19 +101,22 @@ describe('Editor.vue', () => {
     expect(wrapper.find('div[data-test="editor"]').text()).toBe('abc');
   });
 
-  test('The tab key indents with 2 spaces', () => {
+  test('The tab key gets added to the Vue document object', () => {
+    const wrapper = mount(Component);
+    wrapper.trigger('keydown', keyCodes.tab);
+    expect(wrapper.vm.documentData).toBe(tab);
+  });
+
+  test('The tab key gets added to the document', () => {
+    /*
+      Note that .text() trims the resulting string as seen here: https://github.com/vuejs/vue-test-utils/issues/152
+      Therefore we have wrapped the tab in two characters
+    */
     const wrapper = mount(Component);
     wrapper.trigger('keydown', keyCodes.a);
-    wrapper.trigger('keydown', keyCodes.b);
-    wrapper.trigger('keydown', keyCodes.c);
-    wrapper.trigger('keydown.enter');
     wrapper.trigger('keydown', keyCodes.tab);
-    wrapper.trigger('keydown', keyCodes[1]);
-    wrapper.trigger('keydown', keyCodes[2]);
-    wrapper.trigger('keydown', keyCodes[3]);
-    expect(wrapper.find('div[data-test="editor"]').text()).toBe(
-      'abc\n\xa0\xa0123',
-    );
+    wrapper.trigger('keydown', keyCodes.a);
+    expect(wrapper.find('div[data-test="editor"]').text()).toBe(`a${tab}a`);
   });
 });
 
