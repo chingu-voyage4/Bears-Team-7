450297a52bf5974bf155ac36f2cca1d71744fe9e
Matthew-Burfield <m1.burfield@gmail.com>
2018-03-29 13:30:19 +1000

    updated surge to be a dev dependency instead of a prod dependency added some helper comments to the utility functions

--- client/src/components/Home/components/utilities.js
+++ client/src/components/Home/components/utilities.js
@@ -12,12 +12,16 @@ export const updateDocumentData = (
     // backspace
     case 8:
       if (newDocument.length === 1 && newDocument[0] === '') {
+        // If we are at the very start of the document
+        // i.e. the document is empty. Don't remove anything else
         return newDocument;
       }
       if (newDocument[currentRowIndex - 1] === '') {
+        // If we are at the start of a new line, remove the line
         newDocument.splice(currentRowIndex, 1);
         return newDocument;
       }
+      // Otherwise remove the next character on the line
       newDocument[currentRowIndex - 1] = newDocument[currentRowIndex - 1].slice(
         0,
         -1,
@@ -41,8 +45,10 @@ export const setRowNumber = (rowValue, currentRowNumber, { keyCode }) => {
     // backspace
     case 8:
       if (currentRowNumber === 1 && rowValue === '') {
+        // If we are at the start of the document, we don't want to decrease the row number
         return currentRowNumber;
       }
+      // Otherwise reduce the row number if were are at the start of the row
       return rowValue === '' ? currentRowNumber - 1 : currentRowNumber;
     default:
       return currentRowNumber;
