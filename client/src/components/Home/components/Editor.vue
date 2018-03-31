26450d4c439a2767cbf7310c282ee429858bd2eb
AlysCole <anqa071599@gmail.com>
2018-04-01 00:59:11 +0800

    Add ability to move caret by mouse selection
    The user should now be able to move the caret by clicking within the
    editor.
    
     - The whitespaces (new lines and spaces) between the <div> tags and
       {{documentData}} were being included in the inner contents of the
       editor. Reformatted the template to prevent those whitespaces.
     - Changes to the way a new character is inputted to the documentData
       to append the character at the position of the caret.
     - Changes to the way a character is deleted (through the backspace)
       to delete the character at the position of the caret.

--- client/src/components/Home/components/Editor.vue
+++ client/src/components/Home/components/Editor.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="editor" @keydown.prevent="handleKeyboardPress" tabindex="0" data-test="editor">
-    {{ documentData }}
-  </div>
+<div id="editor"
+     class="editor"
+     @keydown.prevent="handleKeyDown"
+     @mouseup="handleMouseUp"
+     tabindex="0"
+     data-test="editor">{{documentData}}</div>
 </template>
 
 <script>
 const getKeyValueToRender = (keyCode, keyValue) => {
   switch (keyCode) {
-    // Enter
     case 13:
+      // Enter
       return '\n';
-    // Tab
     case 9:
+      // Tab
       return '\xa0\xa0';
     default:
       return keyValue;
@@ -22,18 +25,30 @@ export default {
   name: 'Editor',
   data: () => ({
     documentData: '',
-    cursor: {
-      // use a Selection object
+    caret: {
+      // use offset to determine caret position
+      offset: 0,
     },
   }),
   // define methods under the `methods` object
   methods: {
-    handleKeyboardPress(event) {
+    handleKeyDown(event) {
+      // cancel if the control, shift, alt, or meta key is held
+      if (event.ctrlKey || event.shiftKey || event.altKey || event.metaKey) {
+        return;
+      }
+
       // handle key actions
       switch (event.keyCode) {
         // Backspace
         case 8:
-          this.documentData = this.documentData.slice(0, -1);
+          this.documentData =
+            this.documentData.slice(
+              0,
+              Math.max(0, this.caret.offset - 1),
+            ) + this.documentData.slice(this.caret.offset);
+          // move caret to the index before the deleted character
+          this.caret.offset = Math.max(0, this.caret.offset - 1);
           break;
         // Left
         case 37:
@@ -48,10 +63,22 @@ export default {
         case 40:
           break;
         // Text input
-        default: 
+        default:
           this.documentData =
-              this.documentData + getKeyValueToRender(event.keyCode, event.key);
-      
+            this.documentData.slice(0, this.caret.offset) +
+            getKeyValueToRender(event.keyCode, event.key) +
+            this.documentData.slice(this.caret.offset);
+          // move caret to the index after the appended character.
+          this.caret.offset += 1;
+          break;
+      }
+    },
+    handleMouseUp() {
+      const sel = window.getSelection();
+      // check whether selected element is the editor
+      if (document.activeElement === document.getElementById('editor')) {
+        this.caret.offset = sel.anchorOffset;
+      }
     },
   },
 };
@@ -70,6 +97,7 @@ h2 {
   white-space: pre;
   font-size: 16px;
   text-align: left;
+  padding-top: 15px;
   padding-left: 120px;
   overflow: scroll;
 }
